dc97c12e7e947ff42fd5b599edf1e7fbd21ca425
Niall Lindsay <niall_lindsay@icloud.com>
2017-02-14 20:01:06 +0000

    test debug

--- server.py
+++ server.py
@@ -19,10 +19,17 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])#size
         post_data = self.rfile.read(content_length) #data
+        print post_data #Debug only
+        #All for debug       
+        for var in post_data.split("&"):
+            name = var.split("=")[0]
+            value = var.split("=")[1]
+            print "Var: " + name + " : " + value
+        #End debug
         self._set_headers()
         self.wfile.write("Post")
         
-def run(server_class=HTTPServer, handler_class=S, port=80):
+def run(server_class=HTTPServer, handler_class=S, port=3001):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting the http server...'
