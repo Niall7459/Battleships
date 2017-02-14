34b092684485ca42a1d3c0c4b559cc42f09a5979
Niall Lindsay <niall_lindsay@icloud.com>
2017-02-14 19:53:11 +0000

    Create server.py
    Built off of bradmontgomerys version.

---
+++ server.py
@@ -0,0 +1,31 @@
+//Built off of HTTP Server module.
+//WE can use to repsond from the battleships client.
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting the http server...'
+    httpd.serve_forever()
+
+run()
